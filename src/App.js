f041bb9bd269303bca088266ab0fa135e8120506
Sahil <sharma.1901@yahoo.com>
2020-07-20 13:20:41 +0530

    Task Complete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.css";
+import ShowPricing from './Components/ShowPricing/ShowPricing';
+import PriceEdit from './Components/PriceEdit/PriceEdit';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <PriceEdit/>
+      <ShowPricing/>
     </div>
   );
 }
